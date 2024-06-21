cf5cafa915cbbb18c57e234dc6d4083a9439441f
Victor Oliveira <feroli@tuta.io>
2024-06-21 15:30:23 -0300

    feat: create a persisted store

---
+++ src/lib/utils/local-store.svelte.ts
@@ -0,0 +1,43 @@
+import { browser } from '$app/environment';
+
+export class LocalStore<T> {
+	#value = $state<T>() as T;
+	key = '';
+
+	constructor(key: string, value: T) {
+		this.key = key;
+		this.#value = value;
+
+		if (browser) {
+			const item = localStorage.getItem(key);
+			if (item) {
+				this.#value = this.deserialize(item);
+			} else {
+        localStorage.setItem(this.key, this.serialize(this.#value));
+			}
+		}
+	}
+
+	get value() {
+		return this.#value;
+	}
+
+	set value(newValue) {
+		this.#value = newValue;
+		if (browser) {
+			localStorage.setItem(this.key, this.serialize(this.#value));
+		}
+	}
+
+	serialize(value: T): string {
+		return JSON.stringify(value);
+	}
+
+	deserialize(item: string): T {
+		return JSON.parse(item);
+	}
+}
+
+export function localStore<T>(key: string, value: T) {
+	return new LocalStore(key, value);
+}
